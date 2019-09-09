ed13c983be2fd533ad32003dd900793589080c52
Joseph Garnier <garnjose@gmail.com>
2019-09-10 00:29:37 +0200

    add make clean command

--- clean-cmake.sh
+++ clean-cmake.sh
@@ -6,9 +6,7 @@
 
 #!/bin/bash
 
-export PROJECT_NAME="ProjectName" 			# -DPROJECT_NAME: specifies a name for project
-export PROJECT_SUMMARY="Description" 		# -DPROJECT_SUMMARY: short description of the project
-export PROJECT_VENDOR_NAME="YourName" 		# -DPROJECT_VENDOR_NAME: project author
+export PROJECT_NAME="project-name" 			# -DPROJECT_NAME: specifies a name for project
 export PROJECT_VERSION_MAJOR="0" 			# -DPROJECT_VERSION_MAJOR: project major version
 export PROJECT_VERSION_MINOR="0" 			# -DPROJECT_VERSION_MINOR: project minor version
 export PROJECT_VERSION_PATCH="0" 			# -DPROJECT_VERSION_PATCH: project patch version
@@ -18,6 +16,9 @@ declare -r BUILD_PATH="${WORKSPACE_PATH}/build"
 declare -r SOLUTION_PATH="${BUILD_PATH}/${PROJECT_NAME}-${PROJECT_VERSION_MAJOR}-${PROJECT_VERSION_MINOR}-${PROJECT_VERSION_PATCH}-linux"
 
 if [ -d ${SOLUTION_PATH} ]; then
+	cd "${SOLUTION_PATH}"
+	make clean
+	cd "${WORKSPACE_PATH}"
 	rm -r "${SOLUTION_PATH}"
 fi
 
