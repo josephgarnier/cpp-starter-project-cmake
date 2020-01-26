49ee93fbfdd98ea0dac534bfd090d46fdf314514
Joseph Garnier <garnjose@gmail.com>
2020-01-26 17:53:05 +0100

    fix typo and comments

--- cmake/modules/Directory.cmake
+++ cmake/modules/Directory.cmake
@@ -1,48 +1,52 @@
-#! Function for scanning a system of directory.
-#
-# Usage:
-#
-#	scan_sub_folders(
-#		<return_files>
-#		ROOT_PATH <rootpath>
-#		INCLUDE_REGEX [ext1 [ext2 [ext3 ...]]])
-#
-#
-# Arguments:
-#
-#	\return:return_files: the name of the variable that should hold the
-#	returned file list.
-#
-#	\param:ROOT_PATH: specify the root folder to search for files.
-#
-#	\group:INCLUDE_REGEX specify a regular expression that the file names
-#	(without path) must match to be listed.
-#
-#
-# Requires these CMake modules:
-#
-#	None
-#
-# Requires CMake 3.12 or newer
-#
-#
-# Licence:
-#
 # Copyright 2019-present, Joseph Garnier
 # All rights reserved.
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+#[=======================================================================[.rst:
+Directory
+---------
+
+Function for scanning a system of directories.
+
+ Usage
+ ^^^^^
+	scan_sub_folders(
+		<return_files>
+		ROOT_PATH <rootpath>
+		INCLUDE_REGEX [ext1 [ext2 [ext3 ...]]])
+
+
+ Arguments
+ ^^^^^^^^^
+	``\return:return_files``
+	the name of the variable that should hold the returned file list.
+
+	``\param:ROOT_PATH``
+	specify the root folder to search for files.
+
+	``\group:INCLUDE_REGEX``
+	specify a regular expression that the file names (without path)
+	must match to be listed.
+
+
+ Requires these CMake modules
+ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	None
+
+ Requires CMake 3.12 or newer
+ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#]=======================================================================]
 cmake_minimum_required (VERSION 3.12)
 
 function(scan_sub_folders return_files)
 	set(options "")
-	set(oneValueArgs ROOT_PATH INCLUDE_REGEX)
-	set(multiValueArgs "")
-	cmake_parse_arguments(SSF "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+	set(one_value_args ROOT_PATH INCLUDE_REGEX)
+	set(multi_value_args "")
+	cmake_parse_arguments(SSF "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 	if(SSF_UNPARSED_ARGUMENTS)
-      message(FATAL_ERROR "Unrecognized arguments: \"${SSF_UNPARSED_ARGUMENTS}\"")
+		message(FATAL_ERROR "Unrecognized arguments: \"${SSF_UNPARSED_ARGUMENTS}\"")
 	endif()
 	if(NOT DEFINED SSF_ROOT_PATH)
 		set(SSF_ROOT_PATH "")
