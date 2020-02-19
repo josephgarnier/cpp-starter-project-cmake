c923544326d24215b4588aa3b14c767ac355bf7d
Joseph Garnier <garnjose@gmail.com>
2020-02-19 23:21:59 +0100

    add licence header

---
+++ src/sub2/sub2.cpp
@@ -0,0 +1,10 @@
+/******************************************************************************
+* Copyright 2019-present, Joseph Garnier
+* All rights reserved.
+*
+* This source code is licensed under the license found in the
+* LICENSE file in the root directory of this source tree.
+******************************************************************************/
+
+#include "sub2.h"
+#include "project-name_pch.h"
