50c3e81a86339697fff658ac04a0a44cd5b20c20
Joseph Garnier <garnjose@gmail.com>
2020-05-18 11:49:32 +0200

    Rewrite all variable and scanning associated to ProjectSrcFile

--- cmake/project/ProjectSrcFiles.cmake
+++ cmake/project/ProjectSrcFiles.cmake
@@ -6,30 +6,13 @@
 # =============================================================================
 # What Is This?
 # -------------
-# In this files, you will set the lists of all source files of your project.
-# First, you have to set the variable `${PROJECT_NAME}_SRC_ALL_FILES` with
-# source files (cpp and header) present in `${PROJECT_NAME}_SRC_DIR`
-# (src/ directory). Then, set the variable `${PROJECT_NAME}_SRC_HEADER_FILES`
-# with header files present in ${PROJECT_NAME}_SRC_DIR` (src/ directory).
-# Finally, set the variable `${PROJECT_NAME}_INCLUDE_HEADER_FILES` with public
-# header files present in `${PROJECT_NAME}_INCLUDE_DIR/${PROJECT_NAME}/`
-# (this directory is optional). In this variable, only set your own headers
-# files, not headers of dependencies that will add in a specific config file.
-# The last variable `${PROJECT_NAME}_PUBLIC_HEADER_DIRS` is a list of public
-# header directories that will be copy in `include/` directory by `install()` command.
-# By default, this variable is set with all header files of your project ;
-# that's mean all header files are public. Feel free to replace this policy
-# by yours.
+# By default, you don't need to manually initialize these variables: this is
+# done by the glob functions `directory()`. But if after all you decide
+# to use you own instructions, then remove all functions with the exception of 
+# all `set()`. Follow the instructions of each variable to know how initialize them.
 #
-# By default, you don't need to manually initialize these variables : this is
-# done by the blob function `directory()`. But if after all you decide
-# to use you own instructions, then remove the calls to `directory()`
-# function, set the previously described variables, and don't forget to also set
-# `${PROJECT_NAME}_PRECOMPILED_HEADER_FILE` and
-# `${PROJECT_NAME}_PRECOMPILED_SOURCE_FILE`.
-#
-# Warning: if you don't use precompiled header features, set to empty string
-# the variables `${PROJECT_NAME}_PRECOMPILED_HEADER_FILE` and
+# Warning: in all case if you don't use precompiled header features, set to
+# empty string the variables `${PROJECT_NAME}_PRECOMPILED_HEADER_FILE` and
 # `${PROJECT_NAME}_PRECOMPILED_SOURCE_FILE`.
 
 include(Directory)
@@ -38,22 +21,49 @@ include(Directory)
 set(${PROJECT_NAME}_PRECOMPILED_HEADER_FILE "${${PROJECT_NAME}_SRC_DIR}/${PROJECT_NAME}_pch.h")
 set(${PROJECT_NAME}_PRECOMPILED_SOURCE_FILE "${${PROJECT_NAME}_SRC_DIR}/${PROJECT_NAME}_pch.cpp")
 
-# All source files (.cpp and .h) of src/ directory go here
-set(${PROJECT_NAME}_SRC_ALL_FILES "")
-directory(SCAN ${PROJECT_NAME}_SRC_ALL_FILES LIST_DIRECTORIES off RELATIVE off ROOT_DIR "${${PROJECT_NAME}_SRC_DIR}" INCLUDE_REGEX ".*[.]cpp$|.*[.]h$|.*[.]cc$")
+# Only private source files (.cpp) go here, they will not be exported or installed.
+set(${PROJECT_NAME}_SOURCE_PRIVATE_FILES "")
+directory(SCAN ${PROJECT_NAME}_SOURCE_PRIVATE_FILES
+	LIST_DIRECTORIES off
+	RELATIVE off
+	ROOT_DIR "${${PROJECT_NAME}_SRC_DIR}"
+	INCLUDE_REGEX ".*[.]cpp$|.*[.]cc$"
+)
 
-# Only header files of src/ directory go here
-set(${PROJECT_NAME}_SRC_HEADER_FILES "")
-directory(SCAN ${PROJECT_NAME}_SRC_HEADER_FILES LIST_DIRECTORIES off RELATIVE off ROOT_DIR "${${PROJECT_NAME}_SRC_DIR}" INCLUDE_REGEX ".*[.]h$")
+# By default, all header files of `src/` and `include/project-name/` (this
+# last directory is optional) are publics, then these directories are set here.
+# They will be added to the public include_directories property of target.
+# Warning: the header directories of your dependencies should not be set here, they will
+# be added in a specific config file.
+set(${PROJECT_NAME}_HEADER_PUBLIC_DIRS "${${PROJECT_NAME}_SRC_DIR}" "${${PROJECT_NAME}_INCLUDE_DIR}/${PROJECT_NAME}")
 
-# Only cpp files of src/ directiry go here
-set(${PROJECT_NAME}_SRC_SOURCE_FILES "")
-directory(SCAN ${PROJECT_NAME}_SRC_SOURCE_FILES LIST_DIRECTORIES off RELATIVE off ROOT_DIR "${${PROJECT_NAME}_SRC_DIR}" INCLUDE_REGEX ".*[.]cpp$|.*[.]cc$")
+# Set here all header files (.h) present in `${PROJECT_NAME}_HEADER_PUBLIC_DIRS` directories.
+set(${PROJECT_NAME}_HEADER_PUBLIC_FILES "")
+foreach(directory IN ITEMS ${${PROJECT_NAME}_HEADER_PUBLIC_DIRS})
+	set(header_files "")
+	directory(SCAN header_files
+		LIST_DIRECTORIES off
+		RELATIVE off
+		ROOT_DIR "${directory}"
+		INCLUDE_REGEX ".*[.]h$"
+	)
+	list(APPEND ${PROJECT_NAME}_HEADER_PUBLIC_FILES ${header_files})
+endforeach()
 
-# Only you own header files of include/project-name/ directory go here. The headers of your dependencies should not set here,
-# they will be add in a specific config file
-set(${PROJECT_NAME}_INCLUDE_HEADER_FILES "")
-directory(SCAN ${PROJECT_NAME}_INCLUDE_HEADER_FILES LIST_DIRECTORIES off RELATIVE off ROOT_DIR "${${PROJECT_NAME}_INCLUDE_DIR}/${PROJECT_NAME}" INCLUDE_REGEX ".*[.]h$")
+# In this variable, you tell where are the directories of your private header files.
+# Warning: the header directories of your dependencies should not be set here, they will
+# be added in a specific config file.
+set(${PROJECT_NAME}_HEADER_PRIVATE_DIRS "")
 
-# By default, all header files of src/ and include/project-name/ are publics and go here
-set(${PROJECT_NAME}_PUBLIC_HEADER_DIRS "${${PROJECT_NAME}_SRC_DIR}" "${${PROJECT_NAME}_INCLUDE_DIR}/${PROJECT_NAME}")
+# Set here all header files (.h) present in `${PROJECT_NAME}_HEADER_PRIVATE_DIRS` directories.
+set(${PROJECT_NAME}_HEADER_PRIVATE_FILES "")
+foreach(directory IN ITEMS ${${PROJECT_NAME}_HEADER_PRIVATE_DIRS})
+	set(header_files "")
+	directory(SCAN header_files
+		LIST_DIRECTORIES off
+		RELATIVE off
+		ROOT_DIR "${directory}"
+		INCLUDE_REGEX ".*[.]h$"
+	)
+	list(APPEND ${PROJECT_NAME}_HEADER_PRIVATE_FILES ${header_files})
+endforeach()
