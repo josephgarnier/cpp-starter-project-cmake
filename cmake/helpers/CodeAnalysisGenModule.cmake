84aa588ca9448849ebeb8f3d297b7392483207d7
Joseph Garnier <garnjose@gmail.com>
2021-05-01 09:39:12 +0200

    Add the structure for the Code Analysis Generator Module

---
+++ cmake/helpers/CodeAnalysisGenModule.cmake
@@ -0,0 +1,11 @@
+# Copyright 2019-present, Joseph Garnier
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# =============================================================================
+# What Is This?
+# -------------
+# See README file in the root directory of this source tree.
+
+message("!! NOT IMPLEMENTED YET !!")
