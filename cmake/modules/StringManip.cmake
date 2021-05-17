d22fe4aec0fc0244aa48a6ea70be3563d5019685
Joseph Garnier <garnjose@gmail.com>
2021-05-17 08:42:15 +0200

    Rewrite string_manip transform functions and add unit tests

--- cmake/modules/StringManip.cmake
+++ cmake/modules/StringManip.cmake
@@ -15,8 +15,7 @@ Synopsis
 .. parsed-literal::
 
     string_manip(`SPLIT`_ <string> <output_list_var>)
-    string_manip(`TRANSFORM`_ <string_list_var> START_CASE [OUTPUT_VARIABLE <output_list_var>])
-    string_manip(`TRANSFORM`_ <string_var> START_CASE [OUTPUT_VARIABLE <output_var>])
+    string_manip(`TRANSFORM`_ <string_var> <ACTION> [OUTPUT_VARIABLE <output_var>])
     string_manip(`STRIP_INTERFACES`_ <string_var> [OUTPUT_VARIABLE <output_var>])
 
 Usage
@@ -35,18 +34,20 @@ In case where no special char is found, the input string is returned as a list.
 .. _TRANSFORM:
 .. code-block:: cmake
 
-  string_manip(TRANSFORM <string_list_var> START_CASE [OUTPUT_VARIABLE <output_list_var>])
+  string_manip(SPLIT_TRANSFORM <string_var> <ACTION> [OUTPUT_VARIABLE <output_var>])
 
-Transform each element of the list of strings ``<string_list_var>`` into start
-case, storing the result in-place or in the specified ``<output_list_var>`` as a list.
+Apply the string_manip(SPLIT) function on `<string_var>`` and apply an <ACTION>
+to all elements in the list, then, join them and store the result in place or in
+the specified ``<output_var>`` as a string. ``<ACTION>`` is one of:
 
-.. code-block:: cmake
+::
 
-  string_manip(TRANSFORM <string_var> START_CASE [OUTPUT_VARIABLE <output_var>])
+ START_CASE           = The word is converted into start case
+ C_IDENTIFIER_UPPER   = Inspired from string(MAKE_C_IDENTIFIER) CMake function, the
+ word is suffixed by an underscore and converted to upper. If the first character
+ is a digit, an underscore will also be prepended to the result.
 
-Split and transform the string ``<string_var>`` into start case then store de result in
-place or in the specified ``<output_var>`` as a string.
-
+.. _STRIP_INTERFACES:
 .. code-block:: cmake
 
   string_manip(STRIP_INTERFACES <string_var> [OUTPUT_VARIABLE <output_var>])
@@ -62,8 +63,8 @@ cmake_minimum_required (VERSION 3.16)
 #------------------------------------------------------------------------------
 # Public function of this module.
 function(string_manip)
-	set(options START_CASE)
-	set(one_value_args TRANSFORM STRIP_INTERFACES OUTPUT_VARIABLE)
+	set(options START_CASE C_IDENTIFIER_UPPER)
+	set(one_value_args SPLIT_TRANSFORM STRIP_INTERFACES OUTPUT_VARIABLE)
 	set(multi_value_args SPLIT)
 	cmake_parse_arguments(SM "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 	
@@ -73,12 +74,13 @@ function(string_manip)
 
 	if(DEFINED SM_SPLIT)
 		_string_manip_split()
-	elseif(DEFINED SM_TRANSFORM)
-		list(LENGTH ${SM_TRANSFORM} nb_args)
-		if(${nb_args} EQUAL 1)
-			_string_manip_transform_string_var()
+	elseif(DEFINED SM_SPLIT_TRANSFORM)
+		if(${SM_C_IDENTIFIER_UPPER})
+			_string_manip_split_transform_identifier_upper()
+		elseif(${SM_START_CASE})
+			_string_manip_split_transform_start_case()
 		else()
-			_string_manip_transform_string_list()
+			message(FATAL_ERROR "ACTION argument is missing")
 		endif()
 	elseif(DEFINED SM_STRIP_INTERFACES)
 		_string_manip_strip_interfaces()
@@ -107,63 +109,69 @@ endmacro()
 
 #------------------------------------------------------------------------------
 # Internal usage.
-macro(_string_manip_transform_string_list)
+macro(_string_manip_split_transform_identifier_upper)
 	if(DEFINED SM_UNPARSED_ARGUMENTS)
 		message(FATAL_ERROR "Unrecognized arguments: \"${SM_UNPARSED_ARGUMENTS}\"")
 	endif()
-	if(NOT DEFINED SM_TRANSFORM)
-		message(FATAL_ERROR "TRANSFORM argument is missing")
+	if(NOT DEFINED SM_SPLIT_TRANSFORM)
+		message(FATAL_ERROR "TRANSFORM arguments is missing")
 	endif()
-	if(NOT ${SM_START_CASE})
-		message(FATAL_ERROR "START_CASE argument is missing")
+	if(NOT ${SM_C_IDENTIFIER_UPPER})
+		message(FATAL_ERROR "C_IDENTIFIER_UPPER argument is missing")
+	endif()
+	
+	string_manip(SPLIT "${${SM_SPLIT_TRANSFORM}}" word_list)
+	set(output_formated_word "")
+	foreach(word IN ITEMS ${word_list})
+		string(TOUPPER "${word}" formated_word)
+		string(APPEND output_formated_word "_${formated_word}")
+		unset(formated_word)
+	endforeach()
+
+	# The underscore is removed if the second letter is not a digit.
+	string(LENGTH output_formated_word output_formated_word_size)
+	if(${output_formated_word_size} GREATER_EQUAL 2)
+		string(SUBSTRING ${output_formated_word} 1 1 second_letter)
+		if(NOT "${second_letter}" MATCHES "[0-9]")
+			string(SUBSTRING "${output_formated_word}" 1 -1 output_formated_word)
+		endif()
 	endif()
 
-	set(formated_word_list "")
-	foreach(word IN ITEMS ${${SM_TRANSFORM}})
-		string_manip(TRANSFORM word START_CASE OUTPUT_VARIABLE formated_word)
-		list(APPEND formated_word_list "${formated_word}")
-	endforeach()
-	
 	if(NOT DEFINED SM_OUTPUT_VARIABLE)
-		set(${SM_TRANSFORM} "${formated_word_list}" PARENT_SCOPE)
+		set(${SM_SPLIT_TRANSFORM} "${output_formated_word}" PARENT_SCOPE)
 	else()
-		set(${SM_OUTPUT_VARIABLE} "${formated_word_list}" PARENT_SCOPE)
+		set(${SM_OUTPUT_VARIABLE} "${output_formated_word}" PARENT_SCOPE)
 	endif()
 endmacro()
 
 #------------------------------------------------------------------------------
 # Internal usage.
-macro(_string_manip_transform_string_var)
+macro(_string_manip_split_transform_start_case)
 	if(DEFINED SM_UNPARSED_ARGUMENTS)
 		message(FATAL_ERROR "Unrecognized arguments: \"${SM_UNPARSED_ARGUMENTS}\"")
 	endif()
-	if(NOT DEFINED SM_TRANSFORM)
+	if(NOT DEFINED SM_SPLIT_TRANSFORM)
 		message(FATAL_ERROR "TRANSFORM arguments is missing")
 	endif()
 	if(NOT ${SM_START_CASE})
 		message(FATAL_ERROR "START_CASE argument is missing")
 	endif()
 	
-	set(word_list "")
-	set(formated_word "")
-	string_manip(SPLIT "${${SM_TRANSFORM}}" word_list)
-	list(LENGTH word_list nb_args)
-	if(${nb_args} GREATER_EQUAL 2)
-		# The split function returned a list of words
-		string_manip(TRANSFORM word_list START_CASE)
-		list(JOIN word_list "" formated_word)
-	else()
-		# The split function returned a single word
-		string(TOLOWER "${word_list}" formated_word)
+	string_manip(SPLIT "${${SM_SPLIT_TRANSFORM}}" word_list)
+	set(output_formated_word "")
+	foreach(word IN ITEMS ${word_list})
+		string(TOLOWER "${word}" formated_word)
 		string(SUBSTRING ${formated_word} 0 1 first_letter)
 		string(TOUPPER "${first_letter}" first_letter)
 		string(REGEX REPLACE "^.(.*)" "${first_letter}\\1" formated_word "${formated_word}")
-	endif()
+		string(APPEND output_formated_word "${formated_word}")
+		unset(formated_word)
+	endforeach()
 
 	if(NOT DEFINED SM_OUTPUT_VARIABLE)
-		set(${SM_TRANSFORM} "${formated_word}" PARENT_SCOPE)
+		set(${SM_SPLIT_TRANSFORM} "${output_formated_word}" PARENT_SCOPE)
 	else()
-		set(${SM_OUTPUT_VARIABLE} "${formated_word}" PARENT_SCOPE)
+		set(${SM_OUTPUT_VARIABLE} "${output_formated_word}" PARENT_SCOPE)
 	endif()
 endmacro()
 
@@ -186,3 +194,114 @@ macro(_string_manip_strip_interfaces)
 		set(${SM_OUTPUT_VARIABLE} "${string_striped}" PARENT_SCOPE)
 	endif()
 endmacro()
+
+#------------------------------------------------------------------------------
+# Unit Tests.
+function(string_manip_unit_test)
+	#---- string_manip(SPLIT <string> <output_list_var>) ----
+	# Test 1
+	set(my_string "4Alpha-4beTagammaDe9ltaEc/hoalpha-Beta-in_4aLpha_4_be_ta_ga_mm5_aalpha_AlphaBetaGammaDelta")
+	string_manip(SPLIT "${my_string}" output)
+	set(expected "4;Alpha;4be;Tagamma;De9lta;Ec;hoalpha;Beta;in;4a;Lpha;4;be;ta;ga;mm5;aalpha;Alpha;Beta;Gamma;Delta")
+	if (NOT "${output}" STREQUAL "${expected}")
+		message (FATAL_ERROR "SPLIT is \"${output}\", expected is \"${expected}\"")
+	endif()
+	unset(expected)
+	unset(output)
+	unset(my_string)
+	
+	
+	#---- string_manip(SPLIT_TRANSFORM <string_var> START_CASE [OUTPUT_VARIABLE <output_var>]) ----
+	# Test 1
+	set(my_string "4Alpha-4beTagammaDe9ltaEc/hoalpha-Beta-in_4aLpha_4_be_ta_ga_mm5_aalpha_AlphaBetaGammaDelta")
+	string_manip(SPLIT_TRANSFORM my_string START_CASE OUTPUT_VARIABLE output)
+	set(expected "4Alpha4beTagammaDe9ltaEcHoalphaBetaIn4aLpha4BeTaGaMm5AalphaAlphaBetaGammaDelta")
+	if (NOT "${output}" STREQUAL "${expected}")
+		message (FATAL_ERROR "SPLIT_TRANSFORM(START_CASE) is \"${output}\", expected is \"${expected}\"")
+	endif()
+	unset(expected)
+	unset(output)
+	unset(my_string)
+	
+	# Test 2
+	set(output "4Alpha-4beTagammaDe9ltaEc/hoalpha-Beta-in_4aLpha_4_be_ta_ga_mm5_aalpha_AlphaBetaGammaDelta")
+	string_manip(SPLIT_TRANSFORM output START_CASE)
+	set(expected "4Alpha4beTagammaDe9ltaEcHoalphaBetaIn4aLpha4BeTaGaMm5AalphaAlphaBetaGammaDelta")
+	if (NOT "${output}" STREQUAL "${expected}")
+		message (FATAL_ERROR "SPLIT_TRANSFORM(START_CASE) is \"${output}\", expected is \"${expected}\"")
+	endif()
+	unset(expected)
+	unset(output)
+	unset(my_string)
+	
+	# Test 3
+	set(my_string "my-project-name")
+	string_manip(SPLIT_TRANSFORM my_string START_CASE OUTPUT_VARIABLE output)
+	set(expected "MyProjectName")
+	if (NOT "${output}" STREQUAL "${expected}")
+		message (FATAL_ERROR "SPLIT_TRANSFORM(START_CASE) is \"${output}\", expected is \"${expected}\"")
+	endif()
+	unset(expected)
+	unset(output)
+	unset(my_string)
+	
+	
+	#---- string_manip(SPLIT_TRANSFORM <string_var> C_IDENTIFIER_UPPER [OUTPUT_VARIABLE <output_var>]) ----
+	# Test 1
+	set(my_string "4Alpha-4beTagammaDe9ltaEc/hoalpha-Beta-in_4aLpha_4_be_ta_ga_mm5_aalpha_AlphaBetaGammaDelta")
+	string_manip(SPLIT_TRANSFORM my_string C_IDENTIFIER_UPPER OUTPUT_VARIABLE output)
+	set(expected "_4_ALPHA_4BE_TAGAMMA_DE9LTA_EC_HOALPHA_BETA_IN_4A_LPHA_4_BE_TA_GA_MM5_AALPHA_ALPHA_BETA_GAMMA_DELTA")
+	if (NOT "${output}" STREQUAL "${expected}")
+		message (FATAL_ERROR "SPLIT_TRANSFORM(C_IDENTIFIER_UPPER) is \"${output}\", expected is \"${expected}\"")
+	endif()
+	unset(expected)
+	unset(output)
+	unset(my_string)
+	
+	# Test 2
+	set(output "4Alpha-4beTagammaDe9ltaEc/hoalpha-Beta-in_4aLpha_4_be_ta_ga_mm5_aalpha_AlphaBetaGammaDelta")
+	string_manip(SPLIT_TRANSFORM output C_IDENTIFIER_UPPER)
+	set(expected "_4_ALPHA_4BE_TAGAMMA_DE9LTA_EC_HOALPHA_BETA_IN_4A_LPHA_4_BE_TA_GA_MM5_AALPHA_ALPHA_BETA_GAMMA_DELTA")
+	if (NOT "${output}" STREQUAL "${expected}")
+		message (FATAL_ERROR "SPLIT_TRANSFORM(C_IDENTIFIER_UPPER) is \"${output}\", expected is \"${expected}\"")
+	endif()
+	unset(expected)
+	unset(output)
+	unset(my_string)
+	
+	# Test 3
+	set(my_string "alpha")
+	string_manip(SPLIT_TRANSFORM my_string C_IDENTIFIER_UPPER OUTPUT_VARIABLE output)
+	set(expected "ALPHA")
+	if (NOT "${output}" STREQUAL "${expected}")
+		message (FATAL_ERROR "SPLIT_TRANSFORM(C_IDENTIFIER_UPPER) is \"${output}\", expected is \"${expected}\"")
+	endif()
+	unset(expected)
+	unset(output)
+	unset(my_string)
+	
+	# Test 4
+	set(my_string "1alpha")
+	string_manip(SPLIT_TRANSFORM my_string C_IDENTIFIER_UPPER OUTPUT_VARIABLE output)
+	set(expected "_1ALPHA")
+	if (NOT "${output}" STREQUAL "${expected}")
+		message (FATAL_ERROR "SPLIT_TRANSFORM(C_IDENTIFIER_UPPER) is \"${output}\", expected is \"${expected}\"")
+	endif()
+	unset(expected)
+	unset(output)
+	unset(my_string)
+	
+	# Test 5
+	set(my_string "my-project-name")
+	string_manip(SPLIT_TRANSFORM my_string C_IDENTIFIER_UPPER OUTPUT_VARIABLE output)
+	set(expected "MY_PROJECT_NAME")
+	if (NOT "${output}" STREQUAL "${expected}")
+		message (FATAL_ERROR "SPLIT_TRANSFORM(C_IDENTIFIER_UPPER) is \"${output}\", expected is \"${expected}\"")
+	endif()
+	unset(expected)
+	unset(output)
+	unset(my_string)
+	
+	
+	message(STATUS "All tests OK!")
+endfunction()
