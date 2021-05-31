314ba5157ad62f3bef9004324a3059f8e8ebfcef
Joseph Garnier <garnjose@gmail.com>
2021-05-31 17:15:46 +0200

    Add dependency(INCLUDE_DIRECTORIES) and dependency(IMPORTED_LOCATION) commands

--- cmake/modules/Dependency.cmake
+++ cmake/modules/Dependency.cmake
@@ -40,11 +40,37 @@ result or if no file is found. Secondly, when research is successful the `add_li
 CMake function is called and all target properties are filled. To fill in the include
 header files, the variable ``INCLUDE_DIR`` must give where the files are.
 
+.. _INCLUDE_DIRECTORIES:
+.. code-block:: cmake
+
+  dependency(INCLUDE_DIRECTORIES <lib_name> <SET|APPEND> PUBLIC <item_list>...)
+
+Set or append interface include directories to the imported library ``<lib_name>``.
+It works like the ``target_include_directories()`` CMake command
+(see https://cmake.org/cmake/help/latest/command/target_include_directories.html)
+but with a custom behavior for imported dependencies. PUBLIC specifies the scope
+of the following arguments. These one has to use the generator expressions
+``BUILD_INTERFACE`` and ``INSTALL_INTERFACE`` (see https://cmake.org/cmake/help/latest/manual/cmake-buildsystem.7.html#build-specification-with-generator-expressions).
+
+.. _IMPORTED_LOCATION:
+.. code-block:: cmake
+
+  dependency(IMPORTED_LOCATION <lib_name> [CONFIGURATION <config_type>] PUBLIC <item>...)
+
+Set the full path to the imported library ``<lib_name>``. If a ``CONFIGURATION``
+option is given (DEBUG, RELEASE, etc) then the file will only be setted for this
+config type and only if it is a supported configuration. Otherwise it is setted
+for all configuration supported by the imported library. PUBLIC specifies the
+scope of the following arguments. These one has to use the generator expressions
+``BUILD_INTERFACE`` and ``INSTALL_INTERFACE``
+(see https://cmake.org/cmake/help/latest/manual/cmake-buildsystem.7.html#build-specification-with-generator-expressions).
+
 #]=======================================================================]
 include_guard()
 
 cmake_minimum_required (VERSION 3.20)
 include(Directory)
+include(StringManip)
 
 #------------------------------------------------------------------------------
 # Public function of this module.
@@ -60,6 +86,10 @@ function(dependency)
 
 	if(DEFINED DEP_IMPORT)
 		_dependency_import()
+	elseif(DEFINED DEP_INCLUDE_DIRECTORIES)
+		_dependency_include_directories()
+	elseif(DEFINED DEP_IMPORTED_LOCATION)
+		_dependency_imported_location()
 	else()
 		message(FATAL_ERROR "Operation argument is missing")
 	endif()
@@ -151,5 +181,97 @@ macro(_dependency_import)
 			IMPORTED_SONAME_DEBUG "${lib_debug_name}"
 		)
 		set_property(TARGET "${DEP_IMPORT}" APPEND PROPERTY IMPORTED_CONFIGURATIONS "DEBUG")
+	endif()
+endmacro()
+
+
+#------------------------------------------------------------------------------
+# Internal usage.
+macro(_dependency_include_directories)
+	if(NOT DEFINED DEP_INCLUDE_DIRECTORIES)
+		message(FATAL_ERROR "INCLUDE_DIRECTORIES argument is missing or need a value!")
+	endif()
+	if((NOT ${DEP_SET})
+		AND (NOT ${DEP_APPEND}))
+		message(FATAL_ERROR "SET|APPEND argument is missing!")
+	endif()
+	if(${DEP_SET} AND ${DEP_APPEND})
+		message(FATAL_ERROR "SET|APPEND cannot be used together!")
+	endif()
+	if(NOT DEFINED DEP_PUBLIC)
+		OR ("PUBLIC" IN_LIST DEP_KEYWORDS_MISSING_VALUES)
+		message(FATAL_ERROR "PUBLIC argument is missing or need a value!")
+	endif()
+
+	if(NOT TARGET "${DEP_INCLUDE_DIRECTORIES}")
+		message(FATAL_ERROR "The target \"${DEP_INCLUDE_DIRECTORIES}\" does not exists!")
+	endif()
+
+	string_manip(EXTRACT_INTERFACE DEP_PUBLIC BUILD OUTPUT_VARIABLE include_directories_build_interface)
+	string_manip(EXTRACT_INTERFACE DEP_PUBLIC INSTALL OUTPUT_VARIABLE include_directories_install_interface)
+	if(${DEP_SET})
+		set_property(TARGET "${DEP_INCLUDE_DIRECTORIES}"
+			PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${include_directories_build_interface}"
+		)
+		set_property(TARGET "${DEP_INCLUDE_DIRECTORIES}"
+			PROPERTY INTERFACE_INCLUDE_DIRECTORIES_BUILD "${include_directories_build_interface}"
+		)
+		set_property(TARGET "${DEP_INCLUDE_DIRECTORIES}"
+			PROPERTY INTERFACE_INCLUDE_DIRECTORIES_INSTALL "${include_directories_install_interface}"
+		)
+	elseif(${DEP_APPEND})
+		set_property(TARGET "${DEP_INCLUDE_DIRECTORIES}" APPEND
+			PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${include_directories_build_interface}"
+		)
+		set_property(TARGET "${DEP_INCLUDE_DIRECTORIES}" APPEND
+			PROPERTY INTERFACE_INCLUDE_DIRECTORIES_BUILD "${include_directories_build_interface}"
+		)
+		set_property(TARGET "${DEP_INCLUDE_DIRECTORIES}" APPEND
+			PROPERTY INTERFACE_INCLUDE_DIRECTORIES_INSTALL "${include_directories_install_interface}"
+		)
+	else()
+		message(FATAL_ERROR "Wrong option!")
+	endif()
+endmacro()
+
+#------------------------------------------------------------------------------
+# Internal usage.
+macro(_dependency_imported_location)
+	if(NOT DEFINED DEP_IMPORTED_LOCATION)
+		message(FATAL_ERROR "IMPORTED_LOCATION argument is missing or need a value!")
+	endif()
+	if(NOT (DEFINED DEP_CONFIGURATION)
+		AND	("CONFIGURATION" IN_LIST DEP_KEYWORDS_MISSING_VALUES))
+		message(FATAL_ERROR "CONFIGURATION argument is missing or need a value!")
+	endif()
+	if(NOT DEFINED DEP_PUBLIC)
+		OR ("PUBLIC" IN_LIST DEP_KEYWORDS_MISSING_VALUES)
+		message(FATAL_ERROR "PUBLIC argument is missing or need a value!")
+	endif()
+
+	if(NOT TARGET "${DEP_IMPORTED_LOCATION}")
+		message(FATAL_ERROR "The target \"${DEP_IMPORTED_LOCATION}\" does not exists!")
+	endif()
+
+	get_target_property(supported_config_types "${imported_library}" IMPORTED_CONFIGURATIONS)
+	string_manip(EXTRACT_INTERFACE DEP_PUBLIC BUILD OUTPUT_VARIABLE imported_location_build_interface)
+	string_manip(EXTRACT_INTERFACE DEP_PUBLIC INSTALL OUTPUT_VARIABLE imported_location_install_interface)
+	if(DEFINED DEP_CONFIGURATION)
+		if(NOT "${DEP_CONFIGURATION}" IN_LIST supported_config_types)
+			message(FATAL_ERROR "The build type \"${DEP_CONFIGURATION}\" is not a supported configuration!")
+		endif()
+		set_target_properties("${DEP_IMPORTED_LOCATION}" PROPERTIES
+			IMPORTED_LOCATION_${DEP_CONFIGURATION} "${imported_location_build_interface}"
+			IMPORTED_LOCATION_BUILD_${DEP_CONFIGURATION} "${imported_location_build_interface}"
+			IMPORTED_LOCATION_INSTALL_${DEP_CONFIGURATION} "${imported_location_install_interface}"
+		)
+	else()
+		foreach(config_type IN ITEMS ${supported_config_types})
+			set_target_properties("${DEP_IMPORTED_LOCATION}" PROPERTIES
+				IMPORTED_LOCATION_${config_type} "${imported_location_build_interface}"
+				IMPORTED_LOCATION_BUILD_${config_type} "${imported_location_build_interface}"
+				IMPORTED_LOCATION_INSTALL_${config_type} "${imported_location_install_interface}"
+			)
+		endforeach()
 	endif()
 endmacro()
