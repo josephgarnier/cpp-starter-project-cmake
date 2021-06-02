a832e5997f5e547395ba94594d6019a7fab62a45
Joseph Garnier <garnjose@gmail.com>
2021-06-02 15:34:00 +0200

    Add a command to export a dependency

--- cmake/modules/Dependency.cmake
+++ cmake/modules/Dependency.cmake
@@ -15,6 +15,9 @@ Synopsis
 .. parsed-literal::
 
     dependency(`IMPORT`_ <lib_name> <STATIC|SHARED> [RELEASE_NAME <raw_filename>] [DEBUG_NAME <raw_filename>] ROOT_DIR <directory_path> INCLUDE_DIR <directory_path>)
+    dependency(`EXPORT`_ <lib_name> <BUILD_TREE|INSTALL_TREE> [APPEND] OUTPUT_FILE <file_path>)
+    dependency(`INCLUDE_DIRECTORIES`_ <lib_name> <SET|APPEND> PUBLIC <item_list>...)
+    dependency(`IMPORTED_LOCATION`_ <lib_name> [CONFIGURATION <build_type>] PUBLIC <item>...)
 
 Usage
 ^^^^^
@@ -39,6 +42,22 @@ Windows, .so for shared on Linux). An error message occured if there is more tha
 result or if no file is found. Secondly, when research is successful the `add_library(IMPORTED)``
 CMake function is called and all target properties are filled. To fill in the include
 header files, the variable ``INCLUDE_DIR`` must give where the files are.
+
+.. _EXPORT:
+.. code-block:: cmake
+
+  dependency(EXPORT <lib_name> <BUILD_TREE|INSTALL_TREE> [APPEND] OUTPUT_FILE <file_path>)
+
+Export imported library target ``<lib_name>`` from the build-tree or the install-tree
+for a use by outside projects. It includes the features customized of the ``export()``
+for ``BUILD_TREE`` and the ``install(EXPORT)`` and ``install(TARGETS)`` CMake commands
+for ``INSTALL_TREE`` (see https://cmake.org/cmake/help/latest/command/export.html and
+https://cmake.org/cmake/help/latest/command/install.html#export) for imported dependencies.
+The command will create a file ``<file_path>`` that may be included by outside projects to
+import targets from the current project's build-tree or install-tree. This file will be create
+in ``CMAKE_CURRENT_BINARY_DIR`` for ``BUILD_TREE`` and in ``CMAKE_CURRENT_BINARY_DIR/CMakeFiles/Export"
+for ``INSTALL_TREE``. If the ``APPEND`` option is given the generated code will be appended
+to the file instead of overwriting it.
 
 .. _INCLUDE_DIRECTORIES:
 .. code-block:: cmake
@@ -86,6 +105,8 @@ function(dependency)
 
 	if(DEFINED DEP_IMPORT)
 		_dependency_import()
+	elseif(DEFINED DEP_EXPORT)
+		_dependency_export()
 	elseif(DEFINED DEP_INCLUDE_DIRECTORIES)
 		_dependency_include_directories()
 	elseif(DEFINED DEP_IMPORTED_LOCATION)
@@ -184,6 +205,159 @@ macro(_dependency_import)
 	endif()
 endmacro()
 
+#------------------------------------------------------------------------------
+# Internal usage.
+macro(_dependency_export)
+	if(NOT DEFINED DEP_EXPORT)
+		message(FATAL_ERROR "EXPORT argument is missing or need a value!")
+	endif()
+	if((NOT ${DEP_BUILD_TREE})
+		AND (NOT ${DEP_INSTALL_TREE}))
+		message(FATAL_ERROR "BUILD_TREE|INSTALL_TREE argument is missing!")
+	endif()
+	if(${DEP_BUILD_TREE} AND ${DEP_INSTALL_TREE})
+		message(FATAL_ERROR "BUILD_TREE|INSTALL_TREE cannot be used together!")
+	endif()
+	if(NOT DEFINED DEP_OUTPUT_FILE)
+		message(FATAL_ERROR "OUTPUT_FILE argument is missing or need a value!")
+	endif()
+	if(NOT TARGET "${DEP_EXPORT}")
+		message(FATAL_ERROR "The target \"${DEP_EXPORT}\" does not exists!")
+	endif()
+
+	cmake_path(SET export_temp_file "${CMAKE_CURRENT_BINARY_DIR}")
+	cmake_path(SET export_file "${CMAKE_CURRENT_BINARY_DIR}")
+	if(${DEP_BUILD_TREE})
+		cmake_path(APPEND export_temp_file "DependencyBuildTreeTemp.cmake")
+	elseif(${DEP_INSTALL_TREE})
+		cmake_path(APPEND export_temp_file "DependencyInstallTreeTemp.cmake")
+		cmake_path(APPEND export_file "CMakeFiles" "Export")
+	endif()
+	cmake_path(APPEND export_file "${DEP_OUTPUT_FILE}")
+
+	# When cmake command is call, the previous generated files has to be removed.
+	if(EXISTS "${export_file}")
+		file(REMOVE
+			"${export_file}"
+			"${export_temp_file}"
+		)
+	endif()
+	
+	set(import_instructions "")
+	if((EXISTS "${export_temp_file}") AND (NOT ${DEP_APPEND}))
+		message(FATAL_ERROR "Export command already specified for the file \"${export_file}\". Did you miss 'APPEND' keyword?")
+	endif()
+	if(NOT EXISTS "${export_temp_file}")
+		# Ouptut file will be generated only one time after processing all of a project's CMakeLists.txt files.
+		file(GENERATE OUTPUT "${export_file}" 
+			INPUT "${export_temp_file}"
+			TARGET "${DEP_EXPORT}"
+		)
+		if(${DEP_INSTALL_TREE})
+			cmake_path(GET DEP_OUTPUT_FILE PARENT_PATH install_export_dir)
+			install(FILES "${export_file}"
+				DESTINATION "${install_export_dir}"
+			)
+		endif()
+		_export_generate_header_code()
+	endif()
+
+	if(${DEP_BUILD_TREE})
+		_export_generate_build_tree()
+	elseif(${DEP_INSTALL_TREE})
+		_export_generate_install_tree()
+	endif()
+	_export_generate_footer_code()
+	
+	file(APPEND "${export_temp_file}" "${import_instructions}")
+endmacro()
+
+#------------------------------------------------------------------------------
+# Internal usage.
+macro(_export_generate_build_tree)
+	# Create the imported target.
+	get_target_property(target_type "${DEP_EXPORT}" TYPE)
+  	string(APPEND import_instructions "# Create imported target \"$<TARGET_PROPERTY:NAME>\"\n")
+	if("${target_type}" STREQUAL "STATIC_LIBRARY")
+		string(APPEND import_instructions "add_library(\"$<TARGET_PROPERTY:NAME>\" STATIC IMPORTED)\n")
+	elseif("${target_type}" STREQUAL "SHARED_LIBRARY")
+		string(APPEND import_instructions "add_library(\"$<TARGET_PROPERTY:NAME>\" SHARED IMPORTED)\n")
+	else()
+		message(FATAL_ERROR "Target type \"${target_type}\" is unsupported by export command!")
+	endif()
+
+	# Add usage requirements.
+	string(APPEND import_instructions "\n")
+	string(APPEND import_instructions "set_target_properties(\"$<TARGET_PROPERTY:NAME>\" PROPERTIES\n")
+	string(APPEND import_instructions "  INTERFACE_INCLUDE_DIRECTORIES \"$<TARGET_PROPERTY:INTERFACE_INCLUDE_DIRECTORIES_BUILD>\"\n")
+	string(APPEND import_instructions "  IMPORTED_LOCATION_$<CONFIG> \"$<TARGET_PROPERTY:IMPORTED_LOCATION_BUILD_$<CONFIG>>\"\n")
+	string(APPEND import_instructions "  IMPORTED_IMPLIB_$<CONFIG> \"$<TARGET_PROPERTY:IMPORTED_IMPLIB_$<CONFIG>>\"\n")
+	string(APPEND import_instructions "  IMPORTED_SONAME_$<CONFIG> \"$<TARGET_PROPERTY:IMPORTED_SONAME_$<CONFIG>>\"\n")
+	string(APPEND import_instructions ")\n")
+	string(APPEND import_instructions "set_property(TARGET \"$<TARGET_PROPERTY:NAME>\" APPEND PROPERTY IMPORTED_CONFIGURATIONS \"$<CONFIG>\")\n")
+endmacro()
+
+#------------------------------------------------------------------------------
+# Internal usage.
+macro(_export_generate_install_tree)
+	# Add code to compute the installation prefix relative to the import file location.
+	string(APPEND import_instructions "# Compute the installation prefix relative to this file.\n")
+	string(APPEND import_instructions "get_filename_component(_IMPORT_PREFIX \"\${CMAKE_CURRENT_LIST_FILE}\" PATH)\n")
+	set(import_prefix "${install_export_dir}")
+	while((NOT "${import_prefix}" STREQUAL "${CMAKE_INSTALL_PREFIX}")
+		AND (NOT "${import_prefix}" STREQUAL ""))
+		string(APPEND import_instructions "get_filename_component(_IMPORT_PREFIX \"\${_IMPORT_PREFIX}\" PATH)\n")
+		cmake_path(GET import_prefix PARENT_PATH import_prefix)
+	endwhile()
+	string(APPEND import_instructions "if(_IMPORT_PREFIX STREQUAL \"/\")\n")
+	string(APPEND import_instructions "  set(_IMPORT_PREFIX \"\")\n")
+	string(APPEND import_instructions "endif()\n")
+	string(APPEND import_instructions "\n")
+
+	# Create the imported target.
+	get_target_property(target_type "${DEP_EXPORT}" TYPE)
+  	string(APPEND import_instructions "# Create imported target \"$<TARGET_PROPERTY:NAME>\"\n")
+	if("${target_type}" STREQUAL "STATIC_LIBRARY")
+		string(APPEND import_instructions "add_library(\"$<TARGET_PROPERTY:NAME>\" STATIC IMPORTED)\n")
+	elseif("${target_type}" STREQUAL "SHARED_LIBRARY")
+		string(APPEND import_instructions "add_library(\"$<TARGET_PROPERTY:NAME>\" SHARED IMPORTED)\n")
+	else()
+		message(FATAL_ERROR "Target type \"${target_type}\" is unsupported by export command!")
+	endif()
+
+	# Add usage requirements.
+	string(APPEND import_instructions "\n")
+	string(APPEND import_instructions "set_target_properties(\"$<TARGET_PROPERTY:NAME>\" PROPERTIES\n")
+	string(APPEND import_instructions "  INTERFACE_INCLUDE_DIRECTORIES \"\${_IMPORT_PREFIX}/$<TARGET_PROPERTY:INTERFACE_INCLUDE_DIRECTORIES_INSTALL>\"\n")
+	string(APPEND import_instructions "  IMPORTED_LOCATION_$<CONFIG> \"\${_IMPORT_PREFIX}/$<TARGET_PROPERTY:IMPORTED_LOCATION_INSTALL_$<CONFIG>>\"\n")
+	string(APPEND import_instructions "  IMPORTED_IMPLIB_$<CONFIG> \"$<TARGET_PROPERTY:IMPORTED_IMPLIB_$<CONFIG>>\"\n")
+	string(APPEND import_instructions "  IMPORTED_SONAME_$<CONFIG> \"$<TARGET_PROPERTY:IMPORTED_SONAME_$<CONFIG>>\"\n")
+	string(APPEND import_instructions ")\n")
+	string(APPEND import_instructions "set_property(TARGET \"$<TARGET_PROPERTY:NAME>\" APPEND PROPERTY IMPORTED_CONFIGURATIONS \"$<CONFIG>\")\n")
+	string(APPEND import_instructions "\n")
+	
+	# Cleanup temporary variables.
+	string(APPEND import_instructions "# Cleanup temporary variables.\n")
+	string(APPEND import_instructions "unset(_IMPORT_PREFIX)\n")
+endmacro()
+
+#------------------------------------------------------------------------------
+# Internal usage.
+macro(_export_generate_header_code)
+	string(APPEND import_instructions "# Generated by the \"${PROJECT_NAME}\" project \n")
+	string(APPEND import_instructions "\n")
+	string(APPEND import_instructions "#----------------------------------------------------------------\n")
+	string(APPEND import_instructions "# Generated CMake target import file for internal libraries.\n")
+	string(APPEND import_instructions "#----------------------------------------------------------------\n")
+	string(APPEND import_instructions "\n")
+endmacro()
+
+#------------------------------------------------------------------------------
+# Internal usage.
+macro(_export_generate_footer_code)
+	string(APPEND import_instructions "#----------------------------------------------------------------\n")
+	string(APPEND import_instructions "\n")
+endmacro()
 
 #------------------------------------------------------------------------------
 # Internal usage.
@@ -240,8 +414,7 @@ macro(_dependency_imported_location)
 	if(NOT DEFINED DEP_IMPORTED_LOCATION)
 		message(FATAL_ERROR "IMPORTED_LOCATION argument is missing or need a value!")
 	endif()
-	if(NOT (DEFINED DEP_CONFIGURATION)
-		AND	("CONFIGURATION" IN_LIST DEP_KEYWORDS_MISSING_VALUES))
+	if("CONFIGURATION" IN_LIST DEP_KEYWORDS_MISSING_VALUES)
 		message(FATAL_ERROR "CONFIGURATION argument is missing or need a value!")
 	endif()
 	if(NOT DEFINED DEP_PUBLIC)
