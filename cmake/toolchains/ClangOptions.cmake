eab6630e80551a2e81f115c06d250c3ce4d95773
Joseph Garnier <garnjose@gmail.com>
2021-06-04 22:09:21 +0200

    Split clang and gcc options into 2 files

--- cmake/toolchains/ClangOptions.cmake
+++ cmake/toolchains/ClangOptions.cmake
@@ -13,12 +13,43 @@
 # @see https://clang.llvm.org/docs/ClangCommandLineReference.html
 # @see https://github.com/lefticus/cppbestpractices/blob/master/02-Use_the_Tools_Available.md
 
-include(GccOptions.cmake)
+# Diagnostic options.
+# @see https://clang.llvm.org/docs/DiagnosticsReference.html
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wall>") # enables all the warnings about constructions
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wextra>") # enables some extra warning flags
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wshadow>") # warn the user if a variable declaration shadows one from a parent context
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wnon-virtual-dtor>") # warn the user if a class with virtual functions has a non-virtual destructor. This helps catch hard to track down memory errors
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wold-style-cast>") # warn for c-style casts
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wcast-align>") # warn for potential performance problem casts
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wunused>") # warn on anything being unused
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Woverloaded-virtual>") # warn if you overload (not override) a virtual function
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wpedantic>") # warn if non-standard C++ is used
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wconversion>") # warn on type conversions that may lose data
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wsign-conversion>") # warn on sign conversions
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wmisleading-indentation>") # warn if indentation implies blocks where blocks do not exist
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wduplicate-decl-specifier>") # warn if duplicate ‘A’ declaration specifier
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wduplicate-enum>") # warn if element A has been implicitly assigned B which another element has been assigned
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wduplicate-method-arg>") # warn if redeclaration of method parameter A
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wduplicate-method-match>") # warn if multiple declarations of method A found and ignored
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wduplicate-protocol>") # warn if duplicate protocol definition of A is ignored
+# add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wlogical-op-parentheses¶>") # warn about logical operations being used where bitwise were probably wanted: ’&&’ within ‘||’ (only from Clang 13)
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wnull-dereference>") # warn if a null dereference is detected
+# add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wuseless-cast>") # warn if you perform a cast to the same type (bug with GTest)
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wdouble-promotion>") # warn if float is implicit promoted to double
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wformat=2>") # warn on security issues around functions that format output (ie printf)
+#add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wlifetime>") # shows object lifetime issues (only special branch of Clang currently)
 
-# Warning options.
-# add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-Wlifetime>") # shows object lifetime issues (only special branch of Clang currently)
+# Debug information generation options.
+# @see https://clang.llvm.org/docs/ClangCommandLineReference.html#debug-information-generation
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-g>") # produces debugging information
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-ggdb3>") # additional debugging info
+
+# Optimization level options.
+# @see https://clang.llvm.org/docs/ClangCommandLineReference.html#optimization-level
+add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-O1>") # optimize
 
 # Target-independent compilation options.
+# @see https://clang.llvm.org/docs/ClangCommandLineReference.html#target-independent-compilation-options
 add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-fdebug-macro>") # emit macro debug information
 add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-fstandalone-debug>") # emit full debug info for all types used by the program
-add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-stdlib=libc++>") # c++ standard library to use
+# add_compile_options("$<$<STREQUAL:${PARAM_BUILD_TYPE},debug>:-stdlib=libc++>") # c++ standard library to use (no more necessary)
---
+++ cmake/toolchains/Windows_clang.cmake
@@ -0,0 +1,41 @@
+# Copyright 2019-present, Joseph Garnier
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# =============================================================================
+# What Is This?
+# -------------
+# See README file in the root directory of this source tree.
+
+# On Unix use command `uname -s`, for Windows write `Windows` OR use command `cmake --system-information`.
+set(CMAKE_SYSTEM_NAME "Windows")
+
+# On Unix use command `uname -r`, for Windows use command `cmake --system-information`.
+set(CMAKE_SYSTEM_VERSION "10.0")
+
+# @see https://cmake.org/cmake/help/latest/variable/CMAKE_SYSTEM.html.
+set(CMAKE_SYSTEM "${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_VERSION}")
+
+# Write `g++ -v` in a terminal.
+set(triple x86_64-w64-mingw32)
+
+# Specify the cross compiler.
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+
+# Where is the target environment.
+list(APPEND CMAKE_FIND_ROOT_PATH "${CMAKE_SOURCE_DIR}")
+
+# Search for programs in the build host directories.
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+
+# Search for libraries and headers in the target directories.
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
+
+# Compiler flags.
+include("${CMAKE_CURRENT_LIST_DIR}/ClangOptions.cmake")
